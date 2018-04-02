12835404009bfa017768d19cd8b9a0915cb7bcd1
Chris <chriswfoster@gmail.com>
2018-04-02 11:52:31 -0500

    add item axios defined, form completed

--- src/components/GroceryList/GroceryList.js
+++ src/components/GroceryList/GroceryList.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 import '../list.css'
 
 class GroceryList extends Component{
+    constructor(){
+        super()
+        this.state = {
+            inputText: "",
+            arrayOfItems: []
+        }
+    }
+
+    handleSubmit(item){
+        axios.post('/api/additem?item='+item)
+    }
+    handleText(val){
+        this.setState({inputText: val})
+    }
 
 render(){
+    const {arrayOfItems} = this.state;
+    
 return(
 <div className="listMainDiv">
-Grocery List
+<form onSubmit={()=> this.handleSubmit(this.state.inputText)}>
+<input onChange={(e)=> this.handleText(e.target.value)}/>
+<input type="submit" text="ADD"/>
+</form>
+
+<div>
+    {arrayOfItems.map((item) => {
+        <p>
+            {item}
+            </p>
+    })}
+</div>
+
+<button> CLEAR ALL </button>
 </div>
 )
 }
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react'
 
 class ToDoList extends Component{
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+
+
 
 render(){
 return(
